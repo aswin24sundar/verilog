be564d9bf45099fafc6707302c81f1788ac8cbee
aswin24sundar <aswin24sundar@gmail.com>
2026-08-05 12:03:39 +0530

    Create bcdadder_tb.v

---
+++ Bcdadder/bcdadder_tb.v
@@ -0,0 +1,15 @@
+module bcdadder_tb;
+reg [3:0]a,b;
+reg cin;
+wire [3:0]sum;
+wire carry;
+bcdadder dut(.a(a),.b(b),.cin(cin),.sum(sum),.carry(carry));
+initial begin
+    $dumpfile("bcdadder.vcd");
+    $dumpvars(0,dut);
+    $monitor("a=%b,b=%b,cin=%b,sum=%b,carry=%b",a,b,cin,sum,carry,);
+    a=4'b1000;b=4'b1000;cin=0;
+    $finish;
+end
+endmodule
+    
