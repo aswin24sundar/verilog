57dd03f7f79dd65b7e149515284e538ed3a46ac1
aswin24sundar <aswin24sundar@gmail.com>
2026-08-14 20:47:15 +0530

    Create encoder8:3_design.v

---
+++ ENCODER 8:3/encoder8:3_design.v
@@ -0,0 +1,14 @@
+module encoder8_3(
+    input[7:0]a,
+    output reg [2:0]y
+);
+integer i;
+always @(*)begin
+    y=3'b000;
+    for(i=0;i<8;i=i+1)begin
+        if(a[i])
+        y=i;
+    end
+end
+endmodule
+    
