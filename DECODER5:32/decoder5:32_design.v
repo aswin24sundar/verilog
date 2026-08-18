3c040ecd3402bee66800326936df3f7cd2432c57
aswin24sundar <aswin24sundar@gmail.com>
2026-08-18 16:21:21 +0530

    Create decoder5:32_design.v

---
+++ DECODER5:32/decoder5:32_design.v
@@ -0,0 +1,16 @@
+module decoder5_32(
+  input [4:0]a,
+  output reg [31:0]y
+);
+  integer i;
+  always @(*)begin
+    y=5'd0;
+    for(i=0;i<32;i=i+1)begin
+      if(i==a)
+        y[i]=1;
+    end
+  end
+endmodule
+        
+      
+
