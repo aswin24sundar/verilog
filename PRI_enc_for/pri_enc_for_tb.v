77baac64cfea6d6903baa9f4fa1ac7d9b9f48810
aswin24sundar <aswin24sundar@gmail.com>
2026-08-19 11:52:06 +0530

    Create pri_enc_for_tb.v

---
+++ PRI_enc_for/pri_enc_for_tb.v
@@ -0,0 +1,15 @@
+module pri_enc_tb;
+  reg[3:0]a;
+  wire[1:0]y;
+  pri_enc dut (.a(a),.y(y));
+  integer i;
+  initial begin
+    $monitor("a=%b,y=%b",a,y);
+    for(i=0;i<16;i=i+1)begin
+      a=i;
+      #5;
+    end
+    $finish;
+  end
+endmodule
+    
